9d7ad8520bb1068f22ef4f3da8fa8485f029bdcc
Vera Rojman <vrojman@protonmail.com>
2021-05-11 13:52:23 +0200

    Lint

--- decidim-core/app/helpers/decidim/focus_mode_helper.rb
+++ decidim-core/app/helpers/decidim/focus_mode_helper.rb
@@ -4,7 +4,7 @@ module Decidim
   # Helpers related to focus mode
   module FocusModeHelper
     def focus_mode(opts, &body)
-      cell(FocusModeCell, nil, opts).() do
+      cell(FocusModeCell, nil, opts).call do
         capture(&body)
       end
     end
